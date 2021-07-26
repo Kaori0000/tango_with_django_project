5ff0fd9ad5255e34b7a4148f714139a6e6dc5831
Kaori Kurokawa <2351397k@student.gla.ac.uk>
2021-07-26 20:58:43 +0100

    ch3 exercises completed

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,10 @@
+import rango
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>") #ch3 ex. added a hyperlink to the about page
+
+#ch3 Exercises 
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>") #ch3 ex. added a hyperlink to the index page
