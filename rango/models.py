93322e9949cf4260821808b7188673fa5eae6ff6
Kaori Kurokawa <2351397k@student.gla.ac.uk>
2021-07-28 11:30:59 +0100

    ch5 1st commit

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+#create a Category class that inherit from Model class
+class Category(models.Model):
+    #the class attribute name is made of chars with max length = 128, and it's unique 
+    name = models.CharField(max_length=128, unique=True) 
+    
+    #adding a nested Meta class with verbose_name_plural attribute to fix the typo on the Django administraion interface
+    class Meta: 
+        verbose_name_plural = 'Categories'
+
+    def __str__(self): #toString
+        return self.name
+
+
+
+
+#create a Page class that inherit from Model class
+class Page(models.Model):
+     # Category and Page have a 1:M relationthip, thus model.ForeignKey
+      # delete the Page associated with the Category, when Category is deleted 
+    category = models.ForeignKey(Category, on_delete= models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default= 0)
+
+    def __str__(self):
+        return self.title
