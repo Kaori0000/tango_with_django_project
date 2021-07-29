48bb8c8a28d970d0a899ad288d762018831c450d
Kaori0000 <87942116+Kaori0000@users.noreply.github.com>
2021-07-29 12:25:05 +0100

    ch 7 completed

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,4 @@
+from threading import TIMEOUT_MAX
 from django.db import models
 from django.template.defaultfilters import slugify #import the function slugify from Django
 
@@ -5,8 +6,10 @@ from django.template.defaultfilters import slugify #import the function slugify 
 
 #create a Category class that inherit from Model class
 class Category(models.Model):
+    NAME_MAX_LENGTH = 128 #to avoid repeating the code
+
     #the class attribute name is made of chars with max length = 128, and it's unique 
-    name = models.CharField(max_length=128, unique=True) 
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True) 
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True) 
@@ -27,10 +30,13 @@ class Category(models.Model):
 
 #create a Page class that inherit from Model class
 class Page(models.Model):
+    TITLE_MAX_LENGTH = 128
+    URL_MAX_LENGTH = 200
+
      # Category and Page have a 1:M relationthip, thus model.ForeignKey
       # delete the Page associated with the Category, when Category is deleted 
     category = models.ForeignKey(Category, on_delete= models.CASCADE)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=TITLE_MAX_LENGTH)
     url = models.URLField()
     views = models.IntegerField(default= 0)
 
